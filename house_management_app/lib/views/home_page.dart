ed739c13b63c6e2c817ed01428e8ff247cb43ff9
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2023-11-18 19:22:49 +0700

    Giao diện màn hình chính

--- house_management_app/lib/views/home_page.dart
+++ house_management_app/lib/views/home_page.dart
@@ -13,9 +13,37 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-    );
+        appBar: AppBar(
+          title: Row(
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                child:
+                    CircleAvatar(backgroundImage: AssetImage("images/h1.png")),
+              ),
+              TweenAnimationBuilder(
+                tween: Tween<double>(begin: 0, end: 1),
+                duration: const Duration(seconds: 1),
+                builder: ((context, value, child) {
+                  return Opacity(
+                    opacity: value,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 20 - value * 20),
+                      child: child,
+                    ),
+                  );
+                }),
+                child: const Text("Home"),
+              ),
+            ],
+          ),
+          backgroundColor: const Color.fromARGB(255, 15, 125, 21),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+          child: Column(
+            children: [],
+          ),
+        ));
   }
 }
