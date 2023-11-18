c18ed28faabe8e7811ba1abd934a75d29261619a
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2023-11-18 15:01:05 +0700

    test

--- house_management_app/lib/main.dart
+++ house_management_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:house_management_app/login_page.dart';
+import 'package:house_management_app/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const HomeScreen(),
     );
   }
 }
