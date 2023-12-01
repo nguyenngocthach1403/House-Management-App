cf86529973cfdaadab7e93397d11ca76154a0753
ducThink <thinhp20503@gmail.com>
2023-12-02 04:32:43 +0700

    Điều khiển đèn bằng App
    Thêm file firebase_service, sửa file google-service

---
+++ house_management_app/lib/firebase_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class FirebaseService {
+  late DatabaseReference _databaseReference;
+
+  FirebaseService(String roomKey) {
+    // ignore: deprecated_member_use
+    _databaseReference = FirebaseDatabase.instance.reference().child(roomKey);
+  }
+
+  Future<void> updateSwitchStatus(bool status) async {
+    try {
+      await _databaseReference.child('status').set(status);
+    } catch (error) {
+      print('Error updating switch status: $error');
+    }
+  }
+
+  Future<void> updateBrightness(double brightness) async {
+    await _databaseReference.update({'brightness': brightness});
+  }
+
+  Future<void> updateAlarmTime(int time) async {
+    await _databaseReference.child('alarmLed').update({'time': time});
+  }
+
+  Future<void> updateAlarmSpeed(int speed) async {
+    await _databaseReference.child('alarmLed').update({'speed': speed});
+  }
+}
