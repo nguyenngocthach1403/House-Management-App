c04698fe1fe72279c16b832ac58ed88e9ab74afb
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2023-12-02 13:40:49 +0700

    changing feature

--- house_management_app/lib/views/feature.dart
+++ house_management_app/lib/views/feature.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Feature extends StatelessWidget {
-  const Feature({super.key, required this.icon});
-  final IconData icon;
+class Feature extends StatefulWidget {
+  Feature({super.key, required this.icon, required this.action});
+  final Icon icon;
+  VoidCallback action;
+
+  @override
+  State<Feature> createState() => _FeatureState();
+}
+
+class _FeatureState extends State<Feature> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-      width: MediaQuery.of(context).size.width / 4 - 3 * 5,
-      height: MediaQuery.of(context).size.width / 4 - 3 * 5,
-      decoration: const BoxDecoration(
-          color: Color.fromRGBO(53, 110, 95, 1),
-          borderRadius: BorderRadius.all(Radius.circular(7))),
-      child: Icon(
-        icon,
-        color: Colors.white,
-        size: 50,
+    return GestureDetector(
+      onTap: widget.action,
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+        width: MediaQuery.of(context).size.width / 4 - 3 * 5,
+        height: MediaQuery.of(context).size.width / 4 - 3 * 5,
+        decoration: const BoxDecoration(
+            color: Color.fromRGBO(53, 110, 95, 1),
+            borderRadius: BorderRadius.all(Radius.circular(7))),
+        child: widget.icon,
       ),
     );
   }
